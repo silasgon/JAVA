fe19cda4d06185ab20e84911ee10fd0d10dcfb48
Silas Gonm <silasidc@gmail.com>
2021-10-29 18:46:23 -0300

    finaly Methods and class

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -8,25 +8,37 @@ public class Data {
 	
 	Data(){
 		
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		//dia = 1;
+		//mes = 1;
+		//ano = 1970;
 		
+		this(1, 1, 1970);
+		
+		//byte, short, int, long => 0
+		//float, double => 0.0
+		//boolean => false
+		//char => '\u0000'
+		
+		//Objeto => null 
 	}
 	
-	Data(int diaInicial, int mesInicial, int anoInicial){
+	Data(int dia, int mes, int ano){
 		
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		final String formato = "%d/%d/%d";
+		return String.format(formato, dia, mes, ano);
 	}
 	
 	void imprimirDataFormatada() {
-		System.out.printf(obterDataFormatada());
+		System.out.printf(this.obterDataFormatada());
 	}
 
+	static void teste() {
+		 
+	}
 }
---
+++ exercicios/src/classe/desafio/Pessoa.java
@@ -0,0 +1,24 @@
+package classe.desafio;
+
+public class Pessoa {
+	
+	String nome;
+	double peso;
+	
+	Pessoa(String nome, double peso){
+		this.nome = nome;
+		this.peso = peso;
+	}
+	
+	//metodo comer(comida)
+	void comer(Comida comida) {
+		if(comida != null) {
+			this.peso += comida.peso;
+		}
+	}
+	
+	String apresentar() {
+		return "Olá eu sou o " + nome + "e tenho " + peso + " Kgs.";
+	}
+
+}
